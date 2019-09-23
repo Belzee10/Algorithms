4d42d82fd5452e6498a93cd0fabd3b5e314f5d9a
Ivan <belzee92@gmail.com>
2019-09-23 15:42:22 -0400

    add 3 more problems

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,5 +1,17 @@
-import lol from '../index.js';
+import {
+  defangingIpAddress,
+  numJewelsInStones,
+  rangeSumBST
+} from '../index.js';
 
-it('should pass', () => {
-  expect(lol(1, 2)).toBe(3);
+test('should return a defanged IP address', () => {
+  expect(defangingIpAddress('1.1.1.1')).toBe('1[.]1[.]1[.]1');
+});
+
+test('should return the count of appareances of arg1 in arg2', () => {
+  expect(numJewelsInStones('aA', 'aAAbbbb')).toBe(3);
+});
+
+test('should sum all values between arg2 and arg3 (inclusive)', () => {
+  expect(rangeSumBST([10, 5, 15, 3, 7, null, 18], 7, 15)).toBe(32);
 });
--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const lol = (a, b) => a + b;
+/**
+ * https://leetcode.com/problems/defanging-an-ip-address/
+ */
+const defangingIpAddress = ip => {
+  return ip.replace(/[.*+?^${}()|[\]\\]/g, '[.]');
+};
 
-export default lol;
+/**
+ * https://leetcode.com/problems/jewels-and-stones/
+ */
+const numJewelsInStones = (J, S) => {
+  let count = 0;
+  J.split('').forEach(j => {
+    S.split('').forEach(s => {
+      if (j === s) count++;
+    });
+  });
+  return count;
+};
+
+/**
+ * https://leetcode.com/problems/range-sum-of-bst/
+ */
+const rangeSumBST = (root, L, R) => {
+  return root.filter(i => i >= L && i <= R).reduce((acc, next) => acc + next);
+};
+
+module.exports = { defangingIpAddress, numJewelsInStones, rangeSumBST };
